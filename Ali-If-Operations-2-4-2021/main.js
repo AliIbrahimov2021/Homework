567b85c0a8eb9e3b6e1dc31ad018f7abf5e65d65
AliIbrahimov2021 <79990571+AliIbrahimov2021@users.noreply.github.com>
2021-04-02 18:06:29 +0400

    Add files via upload

---
+++ Ali-If-Operations-2-4-2021/main.js
@@ -0,0 +1,13 @@
+var time = "Afternoon";
+
+if (time === "Morning"){
+    document.write("Good Morning, The Time is 08:00 am");
+} else if(time === "Afternoon") {
+    document.write("Good afternoon, The Time is 14:00 pm");
+}
+else{
+    document.write(  "Good Evening, The time is 20:00 pm")
+}
+
+
+
