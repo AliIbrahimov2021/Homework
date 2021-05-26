120d8ecf1f4e73e24760320217e25d4979acb9e4
AliIbrahimov2021 <79990571+AliIbrahimov2021@users.noreply.github.com>
2021-05-26 10:26:53 +0400

    Add files via upload

---
+++ Salimo-26-05-2021/main.js
@@ -0,0 +1,51 @@
+let parallax = document.querySelectorAll('.parallax');
+
+window.addEventListener('scroll', () => {
+    let offset = window.pageYOffset;
+
+    parallax.forEach((prllx) => {
+        // console.log(offset- prllx.offsetTop);
+
+        prllx.style.backgroundPositionY = (offset- prllx.offsetTop) * .7 + "px";
+    })
+})
+
+
+
+
+
+
+
+
+//SLIDES
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active1", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active1";
+}
+
+
+
+
+
